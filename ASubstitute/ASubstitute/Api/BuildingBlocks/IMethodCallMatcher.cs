d3d9ad2129b486ab12ea6ebffedb7e90f9e133d9
Marcin Chwedczuk <chwedczukm@yahoo.com>
2019-01-17 20:05:58 +0100

    Remove unused methods from IMethodCallMatcher

--- ASubstitute/ASubstitute/Api/BuildingBlocks/IMethodCallMatcher.cs
+++ ASubstitute/ASubstitute/Api/BuildingBlocks/IMethodCallMatcher.cs
@@ -5,11 +5,6 @@ using System.Text;
 namespace ASubstitute.Api.BuildingBlocks {
     public interface IMethodCallMatcher
     {
-        // TODO: Group into IMethodCall AssertionCall
-        IMethod Method { get; }
-
-        IImmutableList<IArgumentMatcher> ArgumentMatchers { get; }
-
         bool MatchesCall(IMethodCall call);
     }
 }
